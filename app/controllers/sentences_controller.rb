c5923e0ce0fbfd300b3fe750575f98bcd6ee1b45
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2015-04-24 16:22:56 +0900

    Add edit and update routes to Sentence resource

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,9 +1,32 @@
 class SentencesController < ApplicationController
+
+  before_action :set_sentence, only: [:show, :edit, :update]
+
   def index
     @sentences = Sentence.all
   end
 
   def show
-    render text: "show route. id: #{params[:id]}"
+  end
+
+  def edit
+  end
+
+  def update
+    if @sentence.update(sentence_params)
+      redirect_to @sentence, notice: 'Sentence was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def sentence_params
+    params.require(:sentence).permit(:english, :japanese)
+  end
+
+  def set_sentence
+    @sentence = Sentence.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :sentences, only: [:index, :show]
+  resources :sentences, only: [:index, :show, :edit, :update]
 
   get 'answers/start'
   post 'answers/mark'
