0178605587cc09822249c950b7d942c58b407ec2
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2015-04-24 13:54:02 +0900

    Enable to save Count

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -10,7 +10,29 @@ class AnswersController < ApplicationController
     correct_answer = @sentence.english
     @result = Answer.mark(user_answer: @user_answer,
       correct_answer: correct_answer)
-    # render text: [@user_answer, correct_answer, @result].inspect
+
+    update_count(@sentence, @result)
+    unless @sentence.save
+      render text: 'save faild.'
+    end
+
+  end
+
+  private
+
+  def update_count(sentence, result)
+    if sentence.count
+      try = sentence.count.try
+      correct = sentence.count.correct
+    else
+      try = 0
+      correct = 0
+    end
+
+    try += 1
+    correct += 1 if result
+
+    sentence.count = Count.new(try: try, correct: correct)
   end
 
 end
